e88d062bc794582049f896e22b287771cbb2a445
AdrianRR <AdrianRR@users.noreply.github.com>
2024-01-20 14:52:45 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -7,10 +7,10 @@ export BRANCH="twrp-9.0" # the branch of manifest
 
 # about your device
 export DEVICE="veux" # codename used in device tree
-export DT_LINK="https://github.com/youssefnone/android_recovery_samsung_m10lte" # device tree link
+export DT_LINK="https://github.com/twrpdtgen/android_device_xiaomi_veux" # device tree link
 export DT_BRANCH="twrp" # device tree branch
-export VENDOR="samsung" # device manufacturer or vendor
-export TARGET="recoveryimage" # build method: recoveryimage, bootimage, vendorboot
+export VENDOR="xiaomi" # device manufacturer or vendor
+export TARGET="bootimage" # build method: recoveryimage, bootimage, vendorboot
 export TYPE="omni" # recovery type: omni, twrp, something else
 
 # you dont need to edit these
